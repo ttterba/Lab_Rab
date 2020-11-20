10f3d45ae115b5989c23aa87123708afb69ca913
ttterba <71015409+ttterba@users.noreply.github.com>
2020-11-20 17:06:00 +0300

    Add files via upload

---
+++ LAB_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LAB_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+
+            double a1, a2, a3, a4, a5;
+            double s;
+            a1 = Convert.ToDouble(Console.ReadLine());
+            a2 = Convert.ToDouble(Console.ReadLine());
+            a3 = Convert.ToDouble(Console.ReadLine());
+            a4 = Convert.ToDouble(Console.ReadLine());
+            a5 = Convert.ToDouble(Console.ReadLine());
+        
+            if ((a5 == 0) || (a2 - a3 < 0))
+                Console.WriteLine("ERROR");
+            else
+            {
+                s = a1 * Math.Sqrt(a2 - a3) * (a4 / a5);
+                Console.WriteLine(String.Format("{0:0.000}", s));
+            }
+
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+        }
+    }
+}
