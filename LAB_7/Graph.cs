afda3e1a8b40ca8b352e2a50ec04161803d934c8
ttterba <71015409+ttterba@users.noreply.github.com>
2020-11-20 17:14:45 +0300

    Add files via upload

---
+++ LAB_7/Graph.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_7
+{
+    class Graph
+    {
+        private double a, b;
+
+        public Graph()
+        {
+        }
+
+        public Graph(double pA, double pB)
+        {
+            a = pA; b = pB;
+        }
+
+        public static Graph createGraphFromFile()
+        {
+            double aa = Convert.ToDouble(Console.ReadLine());
+            double bb = Convert.ToDouble(Console.ReadLine());
+            return new Graph(aa, bb);
+        }
+
+        public void Load()
+        {
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double getIntegral()
+        {
+            double result;
+            result = ((b * b) / (-2.0)) - ((a * a) / (-2.0));
+            return result;
+        }
+
+        public double getDistance()
+        {
+            double distance;
+            distance = Math.Sqrt(Math.Pow((b - a), 2) + Math.Pow((-b + a), 2));
+            return distance;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("Дан график функции y=-x");
+            Console.WriteLine(string.Format("a = {0:0.00}, b = {1:0.00}", a, b));
+            Console.WriteLine(string.Format("Опредленный интеграл от a до b равен {0:0.00}", this.getIntegral()));
+            Console.WriteLine(string.Format("Расстояние между точками (a, y(a)) и (b, y(b)) равно {0:0.00}", this.getDistance()));
+            Console.WriteLine("--------------------------------------------------------------------------\n");
+        }
+
+        public void Info(ConsoleColor fg, ConsoleColor bgc)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bgc;
+            Console.Clear();
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("Дан график функции y=-x");
+            Console.WriteLine(string.Format("a = {0:0.00}, b = {1:0.00}", a, b));
+            Console.WriteLine(string.Format("Опредленный интеграл от a до b равен {0:0.00}", this.getIntegral()));
+            Console.WriteLine(string.Format("Расстояние между точками (a, y(a)) и (b, y(b)) равно {0:0.00}", this.getDistance()));
+            Console.WriteLine("--------------------------------------------------------------------------\n");
+        }
+    }
+}
---
+++ LAB_7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LAB_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"graph_output.txt");
+            var new_in = new StreamReader(@"graph_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in); 
+#endif
+            Graph g1, g2;
+#if DEBUG
+            g1 = new Graph(6, 17);
+            g1.Info(ConsoleColor.Red, ConsoleColor.White);
+#endif
+
+#if !DEBUG
+            g1 = Graph.createGraphFromFile();
+            g1.Info();
+            g2 = new Graph(3.4, 13.2);
+            g2.Info();
+#endif
+
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#endif
+
+#if DEBUG
+            Console.ReadKey();
+#endif
+        }
+    }
+}
