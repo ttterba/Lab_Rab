041a27916f57f49ab7dad04d6f87cab44849f466
ttterba <71015409+ttterba@users.noreply.github.com>
2020-11-20 17:10:43 +0300

    Add files via upload

---
+++ LAB_6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LAB_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"sphere_output.txt");
+            var new_in = new StreamReader(@"sphere_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Sphere s = new Sphere();
+            s.Load();
+            s.Info();
+
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
---
+++ LAB_6/Sphere.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_6
+{
+    class Sphere
+    {
+        private double r;
+
+        public double GetV() { return 4/3.0 * Math.PI * Math.Pow(r, 3); }
+
+        public double GetS() { return 4 * Math.PI * Math.Pow(r, 2); }
+
+        public double GetD() { return r * 2; }
+
+        public void Info()
+        {
+            String str =
+                "          *****        \n" +
+                "       *         *      \n" +
+                "      *           *     \n" +
+                "      *   СФЕРА   *     \n" +
+                "      *           *     \n" +
+                "       *         *      \n" +
+                "          *****        \n";
+            Console.WriteLine(str);
+            Console.WriteLine(string.Format("R = {0:0.00}", r));
+            Console.WriteLine(string.Format("D = {0:0.00}", GetD()));
+            Console.WriteLine(string.Format("V = {0:0.00}", GetV()));
+            Console.WriteLine(string.Format("S = {0:0.00}", GetS()));
+        }
+
+        public void Load()
+        {
+            r = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
