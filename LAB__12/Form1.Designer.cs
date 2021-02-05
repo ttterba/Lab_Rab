c77b547cbf4d63ad51ce74a933ee1d8c29a03b50
Ilja Artemov <71015409+ttterba@users.noreply.github.com>
2021-02-05 17:36:33 +0300

    Add files via upload

---
+++ LAB__12/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace LAB__12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxD = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxE = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxD
+            // 
+            this.textBoxD.Location = new System.Drawing.Point(182, 34);
+            this.textBoxD.Name = "textBoxD";
+            this.textBoxD.ReadOnly = true;
+            this.textBoxD.Size = new System.Drawing.Size(100, 20);
+            this.textBoxD.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "d (координата курсора по X)";
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.Location = new System.Drawing.Point(182, 76);
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.ReadOnly = true;
+            this.textBoxZ.Size = new System.Drawing.Size(100, 20);
+            this.textBoxZ.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "z (координата курсора по Y)";
+            // 
+            // textBoxE
+            // 
+            this.textBoxE.Location = new System.Drawing.Point(182, 118);
+            this.textBoxE.Name = "textBoxE";
+            this.textBoxE.Size = new System.Drawing.Size(100, 20);
+            this.textBoxE.TabIndex = 4;
+            this.textBoxE.TextChanged += new System.EventHandler(this.textBoxE_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "e";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(182, 159);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 20);
+            this.textBoxY.TabIndex = 6;
+            this.textBoxY.TextChanged += new System.EventHandler(this.textBoxY_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(162, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxE);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxZ);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxD);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxZ;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxE;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ LAB__12/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace LAB__12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        float d, z, value_e, value_y;
+        double A;
+
+        private void textBoxY_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                value_y = System.Convert.ToSingle(textBoxY.Text);
+                A = ComputeValue(d, z, value_e, value_y);
+                Text = GetStringResult(A);
+            }
+            catch
+            {
+                Text = "ERROR";
+            }
+        }
+
+        private void textBoxE_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                value_e = System.Convert.ToSingle(textBoxE.Text);
+                A = ComputeValue(d, z, value_e, value_y);
+                Text = GetStringResult(A);
+            } catch
+            {
+                Text = "ERROR";
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            d = e.X;
+            z = e.Y;
+            A = ComputeValue(d, z, value_e, value_y);
+            Text = GetStringResult(A);
+
+            textBoxD.Text = e.X.ToString();
+            textBoxZ.Text = e.Y.ToString();
+
+        }
+
+
+        double ComputeValue(float d, float z, float e, float y) 
+        {
+            double result = -d * z / Math.Sqrt(Math.Abs(e)) + Math.Abs(Math.Sin(e) + Math.Cos(y));
+            return result;
+        }
+
+        string GetStringResult(double num)
+        {
+            if (Double.IsInfinity(num) || Double.IsNaN(num))
+            {
+                return "ERROR";
+            }
+            else
+            {
+                return string.Format("A = {0:0.00}", num);
+            }
+        }
+    }
+}
